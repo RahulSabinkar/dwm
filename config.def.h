d1f18d56f64610d383c0c4a5a52a928fb37e8c94
Rahul Sabinkar <rahulsabinkar4u@gmail.com>
2020-10-29 22:12:05 +0000

    assigned some apps to open in certain tags

--- config.def.h
+++ config.def.h
@@ -12,8 +12,11 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Code Pro Semibold:pixelsize=16", "JoyPixels:pixelsize=16:antialias=true:autohint=true" };
-static const char dmenufont[]       = "Source Code Pro:size=12";
+static const char *fonts[]          = { 
+    "Source Code Pro Semibold:pixelsize=16",
+    "JoyPixels:pixelsize=16:antialias=true:autohint=true" 
+};
+static const char dmenufont[]       = "Source Code Pro Semibold:size=16";
 // background color
 static const char col_gray1[]       = "#222222";
 // inactive window border color
@@ -22,17 +25,19 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 // current tag and current window color
 static const char col_gray4[]       = "#eeeeee";
-// top bar second color (blue) and active window border color
+// top bar second color (blue)
 static const char col_cyan[]        = "#005577";
+// active window border color
+static const char col_red[]         = "#fc0303";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray4, col_cyan,  col_red   },
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-// static const char *tags[] = { "", "", "", "", "",  "", "", "", "", "" };
+//static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "", "", "", "", "",  "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
--- config.h
+++ config.h
@@ -37,7 +37,8 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-// static const char *tags[] = { "", "", "", "", "",  "", "", "", "", "" };
+//static const char *tags[] = { "", "", "", "", "",  "", "", "", "", "" };
+//static const char *tags[] = { "",""","",""", };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -47,6 +48,8 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "Brave",   NULL,     NULL,           1,         0,          0,          -1,        -1 },
+	{ "Signal",  NULL,     NULL,           9,         0,          0,          -1,        -1 },
 	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "Terminator",NULL,   NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
